2c274ff49433df4ac78112679fbef1ae6dafbfe7
Dũng <trexhundle@gmail.com>
2024-03-01 11:05:54 +0700

    x

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,57 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
-});
+import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. /api routes
+     * 2. /_next (Next.js internals)
+     * 3. /fonts (inside /public)
+     * 4. /examples (inside /public)
+     * 5. all root files inside /public (e.g. /favicon.ico)
+     */
+    '/((?!api|_next|fonts|examples|[\\w-]+\\.\\w+).*)',
+  ],
 };
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+
+  const hostname = req.headers.get('host') as string;
+  const domain = hostname?.split('.').slice(1).join('.');
+
+  /*  You have to replace ".store-front.local" with your own domain if you deploy this example under your domain.
+      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
+      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
+  const currentHost =
+    process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
+      ? hostname
+          .replace(`.${domain}`, '')
+          .replace(`.platformize.vercel.app`, '')
+      : hostname.replace(`.${domain}`, '');
+  // rewrites for app pages
+  if (currentHost == 'app') {
+    if (
+      url.pathname === '/login' &&
+      (req.cookies.get('next-auth.session-token') ||
+        req.cookies.get('__Secure-next-auth.session-token'))
+    ) {
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
+
+    url.pathname = `/app${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  // rewrite root application to `/home` folder
+  if (hostname === domain) {
+    url.pathname = `/home${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  // rewrite everything else to `/_sites/[site] dynamic route
+  url.pathname = `/_sites/${currentHost}${url.pathname}`;
+  return NextResponse.rewrite(url);
+}
